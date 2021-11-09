4db59738f15c18170f6db1dd4d468e030e7dcfe0
Saurabh Siddharth <saurabhsiddharth68>
2021-11-09 16:56:19 +0530

    Added: Append Data At End

--- src/main/java/com/blz/linkedlist/LinkedList.java
+++ src/main/java/com/blz/linkedlist/LinkedList.java
@@ -15,15 +15,32 @@ public class LinkedList {
             newNode.next = temp;
         }
     }
-
+    public void append(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            Node temp = tail;
+            this.tail = newNode;
+            temp.next = newNode;
+        }
+    }
     public void print() {
         if (head == null) {
             System.out.println("Linked List is Empty");
         } else {
             Node temp = head;
             while (temp != null) {
-                System.out.print(temp.data+ " ");
-                temp = temp.next;
+                if(temp.next != null) {
+                    System.out.print(temp.data + " -> ");
+                    temp = temp.next;
+                }
+                else{
+                    System.out.println(temp.data+" -> ");
+                    temp = temp.next;
+                }
             }
         }
     }
--- src/main/java/com/blz/linkedlist/LinkedlistMain.java
+++ src/main/java/com/blz/linkedlist/LinkedlistMain.java
@@ -1,22 +1,21 @@
 package com.blz.linkedlist;
 
+import java.util.Scanner;
+
 public class LinkedlistMain {
     public static void main(String[] args) {
 
-        Node firstNode = new Node(56);
-        Node secondNode = new Node(70);
-        Node thirdNode = new Node(30);
-
-
-        Node head = firstNode;
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-        Node tail = thirdNode;
-
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.data);
-            temp = temp.next;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to add the data at the start\n" +
+                "Enter 2 to add the data at the end");
+        switch(scanner.nextInt()) {
+            case 1:
+                Operation.addDataAtStart();
+                break;
+            case 2:
+                Operation.addDataAtEnd();
+                break;
         }
+        scanner.close();
     }
 }
